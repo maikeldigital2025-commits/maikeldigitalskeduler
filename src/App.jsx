677d2ff9f149f9487ce8e05ae3b3ed2fdcdd9de9
maikeldigital2025-commits <maikeldigital2025@gmail.com>
2025-10-29 15:36:24 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,4 @@
 import React from 'react';
-import Dashboard from './components/Dashboard';
-import './styles/globals.css';
-
-function App() {
-  return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-      <Dashboard />
-    </div>
-  );
+export default function App() {
+  return <div>Hello Maikel Digital Scheduler</div>;
 }
-
-export default App;
